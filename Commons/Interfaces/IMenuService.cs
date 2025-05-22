7d4844c6be67f0095f1f5400ece93bc0abf8d4f1
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 17:11:16 +0200

    Add MenuService

---
+++ Commons/Interfaces/IMenuService.cs
@@ -0,0 +1,15 @@
+﻿using Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface IMenuService
+    {
+        DisplayMenu CreateMainMenu();
+        DisplayMenu CreateShapesMenu();
+    }
+}
---
+++ Commons/Utilities/MenuService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Interfaces;
+using Common.Utilities;
+
+namespace Services
+{
+    public class MenuService : IMenuService 
+    {
+        public DisplayMenu CreateMainMenu()
+        {
+            string title = "Welcome";
+            string[] options = { "Shapes", "Calculator", "Rock, Paper, Scissors", "Exit" };
+            string prompt = "Welcome to the Triple Project!";
+            return new DisplayMenu(title, prompt, options);
+        }
+
+        public DisplayMenu CreateShapesMenu()
+        {
+            string title = "Shapes";
+            string[] options = { "Rectangle", "Parallelogram", "Rhombus", "Triangle", "Back" };
+            string prompt = "Choose a shape to calculate";
+            return new DisplayMenu(title, prompt, options);
+        }
+    }
+}
