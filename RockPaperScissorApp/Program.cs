97cef2e163d24e6127f78fc320f4dafbcd9bac78
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 14:09:58 +0200

    Create Standalone Projects
    - Add Project References

---
+++ RockPaperScissorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RockPaperScissorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
