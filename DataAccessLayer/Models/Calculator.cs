96c85148e66589bcd23ea76ad62248d3e8687e3a
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 15:06:20 +0200

    Add Calculator Model

---
+++ DataAccessLayer/Models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Calculator
+    {
+        public int CalculatorId { get; set; }
+        public decimal Number1 { get; set; }
+        public decimal Number2 { get; set; }
+        public decimal Result { get; set; }
+        public string Operator { get; set; }
+        public DateTime DateOfCalculation { get; set; }
+
+    }
+}
