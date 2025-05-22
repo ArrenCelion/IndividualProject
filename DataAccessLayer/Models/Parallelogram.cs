ac8098f076a1f10e03d66db3f7f10104dbf9db5c
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 15:05:12 +0200

    Add Parallelogram Model

---
+++ DataAccessLayer/Models/Parallelogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Parallelogram
+    {
+        public int ParallelogramId { get; set; }
+        public decimal Base { get; set; }
+        public decimal Height { get; set; }
+        public decimal Side { get; set; }
+        public decimal Area { get; set; }
+        public decimal Circumference { get; set; }
+        public DateOnly DateOfCalculation { get; set; }
+        public bool IsRhombus { get; set; }
+    }
+}
