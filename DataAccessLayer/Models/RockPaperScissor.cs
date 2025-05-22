72bf389dc5ee98f0fdd159c5a82c50b39fa5444e
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 15:06:46 +0200

    Add RPS Model

---
+++ DataAccessLayer/Models/RockPaperScissor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class RockPaperScissor
+    {
+        public int RockPaperScissorId { get; set; }
+        public string PlayerHand { get; set; }
+        public string ComputerHand { get; set; }
+        public string Result { get; set; }
+        public DateTime DateOfGame { get; set; }
+        public decimal GamesWonAverage { get; set; }
+    }
+}
