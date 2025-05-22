687a206919556c4741901722e69f489d541ab35b
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 13:54:31 +0200

    Add DataInitializer

---
+++ DataAccessLayer/DataInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataInitializer
+    {
+        ApplicationDbContext _dbContext;
+        public DataInitializer(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //public void SeedData()
+        //{
+        //    _dbContext.Database.Migrate();
+        //}
+    }
+}
