90cff9f2175bbd9b4490b0795a4061e7c19ac0da
ArrenCelion <rut.nslund@gmail.com>
2025-05-22 13:53:44 +0200

    Add ApplicationDbContext

---
+++ DataAccessLayer/ApplicationDBContext.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        DbSet<RectangleModel> RectangleModels { get; set; }
+        DbSet<Triangle> Triangles { get; set; }
+        DbSet<Parallelogram> Parallelograms { get; set; }
+        DbSet<Calculator> Calculators { get; set; }
+        DbSet<RockPaperScissor> RockPaperScissors { get; set; }
+
+
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.;Database=TrippleProject;Trusted_Connection=True;TrustServerCertificate=true;");
+            } 
+        }
+
+    }
+
+}
